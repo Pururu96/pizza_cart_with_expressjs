1247d162ef73734df1018f7f8a98d052137c0300
Pururu <pururumphahlele@gmail.com>
2021-10-13 14:33:09 +0200

    Update shopping cart.

--- PizzaCartFF.js
+++ PizzaCartFF.js
@@ -1,152 +1,93 @@
-const smallPBtn = document.querySelector(".pBtn.small");
-const medPBtn = document.querySelector(".pBtn.med");
-const largePBtn = document.querySelector(".pBtn.large");
+module.exports = function PizzaCart() {
+    var smallCost = 0;
+    var medCost = 0;
+    var largeCost = 0;
+    
+    var smallPizzaQty = 0;
+    var mediumPizzaQty = 0;
+    var largePizzaQty = 0;
+    var totalCart = 0;
 
-const smallPBtnBuy = document.querySelector(".pBtn.small.buy");
-const medPBtnBuy = document.querySelector(".pBtn.med.buy");
-const largePBtnBuy = document.querySelector(".pBtn.large.buy");
+    function buttonClick(event) {
+        if (event === ".addBtn1") {
+            smallPizzaQty++;
+        } else if (event === ".addBtn2") {
+            mediumPizzaQty++;
+        } else if (event === ".addBtn3") {
+            largePizzaQty++;
+        }
 
-const smallMBtn = document.querySelector(".mBtn.small");
-const medMBtn = document.querySelector(".mBtn.med");
-const largeMBtn = document.querySelector(".mBtn.large");
-
-const smallPizzaQty = document.querySelector(".smallPizzaQty");
-const medPizzaQty = document.querySelector(".medPizzaQty");
-const largePizzaQty = document.querySelector(".largePizzaQty");
-
-const smallPizzaTotal = document.querySelector(".smallPizzaTotal");
-const medPizzaTotal = document.querySelector(".medPizzaTotal");
-const largePizzaTotal = document.querySelector(".largePizzaTotal");
-const cartTotal = document.querySelector(".cartTotal");
-
-const checkOut = document.querySelector(".checkOut");
-
-const payOut = document.querySelector(".payOut");
-const message = document.querySelector(".message");
-const payAmt = document.querySelector(".payAmt");
-const payBtn = document.querySelector(".payBtn");
-
-const containerBox = document.querySelector(".containerBox");
-const closeBtn = document.querySelector(".close");
-
-var width = screen.availWidth;
-var height = screen.availHeight;
-
-// containerBox.style.width = width + 'px';
-// containerBox.style.height = height + 'px';
-// containerBox.style.paddingTop = height * 1 / 3 + 'px';
-
-const cartbill = cart();
-
-function BtnClick(event) {
-    cartbill.btnClickff(event.target.dataset.size);
-
-    smallPizzaQty.innerHTML = cartbill.qtyUpdate().smallQty;
-    medPizzaQty.innerHTML = cartbill.qtyUpdate().medQty;
-    largePizzaQty.innerHTML = cartbill.qtyUpdate().largeQty;
-
-    smallPizzaTotal.innerHTML = cartbill.priceUpdate().smallCost;
-    medPizzaTotal.innerHTML = cartbill.priceUpdate().medCost;
-    largePizzaTotal.innerHTML = cartbill.priceUpdate().largeCost;
-    cartTotal.innerHTML = cartbill.priceUpdate().totalCart;
-
-    if (cartbill.priceUpdate().totalCart > 0) {
-        checkOut.classList.remove('hidden');
-    } else {
-        checkOut.classList.add('hidden');
-        payOut.classList.add('hidden');
+        if (event === ".subBtn1") {
+            smallPizzaQty--;
+            if (smallPizzaQty < 0) {
+                smallPizzaQty = 0;
+            }
+        } else if (event === ".subBtn2") {
+            mediumPizzaQty--;
+            if (mediumPizzaQty < 0) {
+                mediumPizzaQty = 0;
+            }
+        } else if (event === ".subBtn3") {
+            largePizzaQty--;
+            if (largePizzaQty < 0) {
+                largePizzaQty = 0;
+            }
+        }
     }
-}
 
-function checkOutClick() {
-    checkOut.classList.add('hidden');
-    payOut.classList.remove('hidden');
-}
-
-function payment() {
-    containerBox.classList.remove('hidden');
-    var paymentAmt = Number(payAmt.value);
-    if (paymentAmt == cartbill.priceUpdate().totalCart) {
-        containerBox.style.backgroundColor = 'rgba(120, 255, 120, 0.95)';
-        message.innerHTML = "Enjoy your Pizza!";
-        cartbill.resetCart();
-
-        smallPizzaQty.innerHTML = cartbill.resetCart().smallQty;
-        medPizzaQty.innerHTML = cartbill.resetCart().medQty;
-        largePizzaQty.innerHTML = cartbill.resetCart().largeQty;
-
-        smallPizzaTotal.innerHTML = cartbill.resetCart().smallCost;
-        medPizzaTotal.innerHTML = cartbill.resetCart().medCost;
-        largePizzaTotal.innerHTML = cartbill.resetCart().largeCost;
-        cartTotal.innerHTML = cartbill.resetCart().totalCart;
-
-        setTimeout(function () {
-            containerBox.classList.add('hidden');
-            checkOut.classList.remove('hidden');
-            payOut.classList.add('hidden');
-            payAmt.value = "";
-        }, 4500);
-
-    } else if (paymentAmt > cartbill.priceUpdate().totalCart) {
-        containerBox.style.backgroundColor = 'rgba(120, 255, 120, 0.95)';
-        message.innerHTML = "Enjoy your Pizza, here is your change R" + cartbill.change(paymentAmt);
-        cartbill.resetCart();
-
-        smallPizzaQty.innerHTML = cartbill.resetCart().smallQty;
-        medPizzaQty.innerHTML = cartbill.resetCart().medQty;
-        largePizzaQty.innerHTML = cartbill.resetCart().largeQty;
-
-        smallPizzaTotal.innerHTML = cartbill.resetCart().smallCost;
-        medPizzaTotal.innerHTML = cartbill.resetCart().medCost;
-        largePizzaTotal.innerHTML = cartbill.resetCart().largeCost;
-        cartTotal.innerHTML = cartbill.resetCart().totalCart;
-
-        setTimeout(function () {
-            containerBox.classList.add('hidden');
-            checkOut.classList.remove('hidden');
-            payOut.classList.add('hidden');
-            payAmt.value = "";
-        }, 4500);
-
-    } else {
-        containerBox.style.backgroundColor = 'rgba(255, 120, 120, 0.95)';
-        message.innerHTML = "Sorry, that is not enough money!";
-        setTimeout(function () {
-            containerBox.classList.add('hidden');
-        }, 4500);
+    function quantity() {
+        return {
+            smallPizzaQty,
+            mediumPizzaQty,
+            largePizzaQty
+        }
     }
+
+    function cost() {
+        smallCost = (smallPizzaQty * 39).toFixed(2);
+        medCost = (mediumPizzaQty * 79).toFixed(2);
+        largeCost = (largePizzaQty * 99).toFixed(2);
+        totalCart = (smallPizzaQty * 39.00 + mediumPizzaQty * 79.00 + largePizzaQty * 99.00).toFixed(2);
+
+        return {
+            smallCost,
+            medCost,
+            largeCost,
+            totalCart
+        }
+    }
+
+    function change(amount) {
+        return (amount - totalCart).toFixed(2);
+    }
+
+    function resetCart() {
+        smallPizzaQty = 0;
+        mediumPizzaQty = 0;
+        largePizzaQty = 0;
+        smallCost = 0;
+        medCost = 0;
+        largeCost = 0;
+        totalCart = 0;
+
+        return {
+            smallPizzaQty,
+            mediumPizzaQty,
+            largePizzaQty,
+            smallCost,
+            medCost,
+            largeCost,
+            totalCart,
+        }
+    }
+
+    return {
+        buttonClick,
+        quantity,
+        cost,
+        change,
+        resetCart
+    }
+
+
 }
-
-function close() {
-    containerBox.classList.add('hidden');
-}
-
-function resize() { 
-width = screen.availWidth;
-height = screen.availHeight;
-
-containerBox.style.width = width + 'px';
-containerBox.style.height = height + 'px';
-containerBox.style.paddingTop = height * 1 / 3 + 'px';
-}
-
-
-smallPBtn.addEventListener('click', BtnClick);
-smallMBtn.addEventListener('click', BtnClick);
-smallPBtnBuy.addEventListener('click', BtnClick);
-
-medPBtn.addEventListener('click', BtnClick);
-medMBtn.addEventListener('click', BtnClick);
-medPBtnBuy.addEventListener('click', BtnClick);
-
-largePBtn.addEventListener('click', BtnClick);
-largeMBtn.addEventListener('click', BtnClick);
-largePBtnBuy.addEventListener('click', BtnClick);
-
-checkOut.addEventListener('click', checkOutClick);
-
-payBtn.addEventListener('click', payment);
-
-closeBtn.addEventListener('click', close);
-
-window.addEventListener('resize', resize);
--- index.js
+++ index.js
@@ -1,30 +1,46 @@
-var express = require('express');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
 
 var PizzaCart = require('./PizzaCartFF')
+var express = require('express')
 var app = express();
 var pizzaCart = PizzaCart();
 
-//set up view engine
-app.engine('handlebars', exphbs({ 
+// set up view engine
+app.engine('handlebars', exphbs({
     defaultLayout: 'main',
-    layoutsDir : './views/layouts'
+    layoutsDir: './views/layouts'
 }));
 app.set('view engine', 'handlebars');
 
 // parse application/x -www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
 // parse application/json
 app.use(bodyParser.json())
 
 app.use(express.static('public'))
 
+var counter = 0;
 
+app.get('/', function (req, res) {
+    res.render('index', {
+        cost: pizzaCart.cost(),
+        quantity: pizzaCart.quantity()
+    })
+}
+    , app.post('/pizzacart', function (req, res) {
+        pizzaCart.cost(req.body);
+        res.redirect('/')
+    })
 
-
-
+    , app.post('/count', function (req, res) {
+        counter++;
+        res.redirect('/')
+    }));
 
 const PORT = process.env.PORT || 3009;
 
