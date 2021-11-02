1bb19d6122d6027460827458b9ac6aac8a870900
Pururu <pururumphahlele@gmail.com>
2021-11-02 14:05:40 +0200

    Converted JS module to an ES6 module.

--- test/pizzacart-test.js
+++ test/pizzacart-test.js
@@ -1,25 +1,25 @@
-var cart = require('./PizzaCartFF');
-var assert = require("assert");
+import cart from './PizzaCartFF';
+import { equal } from "assert";
 
 describe('The Small Pizza price', function(){
     it('should reflect as 49.00 when the a small pizza is purchased', function(){
         const pizza_cart = cart();
         pizza_cart.buttonClick('small');
-        assert.equal(49.00, totals.smallCost());
+        equal(49.00, totals.smallCost());
     
 });
 describe('The Medium pizza price', function(){
 it('should reflect as 89.00 when a medium pizza is purchased', function(){
     const pizza_cart = cart();
     pizza_cart.buttonClick('medium');
-    assert.equal(89.00, totals.mediumCost());
+    equal(89.00, totals.mediumCost());
 });
 
 describe('The Large pizza price', function(){
     it('should reflect as 129.00 when a large pizza is purchased', function(){
         const pizza_cart = cart();
         pizza_cart.buttonClick('large'); 
-        assert.equal(129.00, totals.largeCost());
+        equal(129.00, totals.largeCost());
     });
     
 });
